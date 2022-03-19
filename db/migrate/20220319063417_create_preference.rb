f7e6475b15822b465b629bfd785e0d67a5ed0571
braum852 <julian.teo59@yahoo.com>
2022-03-19 16:35:39 +1000

    Done

---
+++ db/migrate/20220319063417_create_preference.rb
@@ -0,0 +1,9 @@
+class CreatePreference < ActiveRecord::Migration[5.0]
+  def change
+    create_table :preferences do |t|
+      t.boolean :allow_create_songs
+      t.boolean :allow_create_artists
+      t.timestamps
+    end
+  end
+end
